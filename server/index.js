5bd9532c37ef8237974943be442008a4b4f4d354
Tanner Velten <tannervelten@gmail.com>
2018-03-01 15:34:32 -0800

    readme, renamed to client

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ if (cluster.isMaster) {
   const app = express();
 
   // Priority serve any static files.
-  app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+  app.use(express.static(path.resolve(__dirname, '../client/build')));
 
   // Answer API requests.
   app.get('/api', function (req, res) {
@@ -32,7 +32,7 @@ if (cluster.isMaster) {
 
   // All remaining requests return the React app, so it can handle routing.
   app.get('*', function(request, response) {
-    response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+    response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
   });
 
   app.listen(PORT, function () {
