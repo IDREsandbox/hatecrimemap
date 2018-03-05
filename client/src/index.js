45a1f051d68341b54c6f75e218ed046fbbd9e280
Tanner Velten <tannervelten@gmail.com>
2018-03-05 15:47:46 -0800

    eslint

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-/* eslint-disable */
-
 ReactDOM.render(
   <App />,
   document.getElementById('root'),
 );
-
-/* eslint-enable */
