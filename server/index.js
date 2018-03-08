aad804f3bee9deebabb63610e7ee82e791306498
Tanner Velten <tannervelten@gmail.com>
2018-03-07 16:37:42 -0800

    cleanup

--- server/index.js
+++ server/index.js
@@ -7,34 +7,34 @@ const { api, getmapdata } = require('./queries');
 
 const PORT = process.env.PORT || 5000;
 
-// Multi-process to utilize all CPU cores.
+// Master process is started from the command line
 if (cluster.isMaster) {
-  console.error(`Node cluster master ${process.pid} is running`);
+  console.log(`Node cluster master ${process.pid} is running`);
 
-  // Fork workers.
+  // Create worker for each CPU
   for (let i = 0; i < numCPUs; i++) {
     cluster.fork();
   }
 
   cluster.on('exit', (worker, code, signal) => {
-    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
+    console.log(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
+    cluster.fork();
   });
 } else {
   const app = express();
 
-  // Priority serve any static files.
+  // Priority serve any static files
   app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-  // Answer API requests.
   app.get('/api', api);
   app.get('/api/mapdata', getmapdata);
 
-  // All remaining requests return the React app, so it can handle routing.
+  // All remaining requests return the React app, so it can handle routing
   app.get('*', (request, response) => {
     response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
   });
 
   app.listen(PORT, () => {
-    console.error(`Node cluster worker ${process.pid}: listening on port ${PORT}`);
+    console.log(`Node cluster worker ${process.pid} listening on port ${PORT}`);
   });
 }
