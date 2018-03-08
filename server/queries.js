dd355baa12f15ad41bc6f4dacd642c4ffc8f02fd
Tanner Velten <tannervelten@gmail.com>
2018-03-07 17:33:48 -0800

    conditionally setup connection based on environment

--- server/queries.js
+++ server/queries.js
@@ -5,8 +5,13 @@ const options = {
 require('dotenv').config();
 const pgp = require('pg-promise')(options);
 
-pgp.pg.defaults.ssl = true;
-const connectionString = process.env.DATABASE_URL;
+let connectionString;
+if (process.env.NODE_ENV !== 'development') {
+  connectionString = process.env.DATABASE_URL;
+  pgp.pg.defaults.ssl = true;
+} else {
+  connectionString = process.env.LOCALDB_URL;
+}
 const db = pgp(connectionString);
 
 function api(req, res) {
