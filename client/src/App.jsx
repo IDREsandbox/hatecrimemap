3783e2adb54f4af1848688641d533dfd508ae733
Tanner Velten <tannervelten@gmail.com>
2018-03-10 15:00:08 -0800

    renamed map component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-import SimpleMap from './components/Map/Map';
+import SimpleMap from './components/SimpleMap/SimpleMap';
 
 export default class App extends Component {
   constructor(props) {
