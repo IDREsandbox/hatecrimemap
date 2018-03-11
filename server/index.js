a262c6d439a02d84941943d8dfc575372742fa1f
Tanner Velten <tannervelten@gmail.com>
2018-03-10 22:20:23 -0800

    refactoring

--- server/index.js
+++ server/index.js
@@ -5,14 +5,13 @@ const numCPUs = require('os').cpus();
 
 const routes = require('./controllers');
 
-const PORT = process.env.PORT || 5000;
-
 // Master process is started from the command line
 if (cluster.isMaster) {
   numCPUs.forEach(() => cluster.fork());
   console.log('Server running...');
 } else {
   const app = express();
+  const PORT = process.env.PORT || 5000;
 
   // Priority serve any static files
   app.use(express.static(path.resolve(__dirname, '../client/build')));
