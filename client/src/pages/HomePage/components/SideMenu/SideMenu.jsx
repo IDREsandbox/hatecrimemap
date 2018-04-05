33b46d2e0cb49d89a5ed31a23f9fdf0bd373443e
Tanner Velten <tannervelten@gmail.com>
2018-04-05 10:23:28 -0700

    filter menu

--- client/src/pages/HomePage/components/SideMenu/SideMenu.jsx
+++ client/src/pages/HomePage/components/SideMenu/SideMenu.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import FilterMenu from './components/FilterMenu/FilterMenu';
+import FilterMenu from '../../../../components/FilterMenu/FilterMenu';
 import './SideMenu.css';
 
 const SideMenu = ({ updateMapData }) => (
