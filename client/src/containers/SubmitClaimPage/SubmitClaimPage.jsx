4038539a38106b55039340eb5a779c0d80435315
Tanner Velten <tannervelten@gmail.com>
2018-04-19 17:37:33 -0700

    added react bootstrap

---
+++ client/src/containers/SubmitClaimPage/SubmitClaimPage.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import './SubmitClaimPage.css';
+
+class SubmitClaimPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      groupsHarassed: [],
+    };
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+SubmitClaimPage.propTypes = {
+
+};
+
+export default SubmitClaimPage;
