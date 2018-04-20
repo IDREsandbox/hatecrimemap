d1ada4ee7bb3aff49f02e30186591d3ec0c29258
Tanner Velten <tannervelten@gmail.com>
2018-04-19 18:03:52 -0700

    moved routes to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import HomePage from './containers/HomePage/HomePage';
-// import Header from './components/Header/Header';
+import Header from './components/Header/Header';
+import SubmitClaimPage from './containers/SubmitClaimPage/SubmitClaimPage';
 import './App.css';
 
 const App = () => (
-  <div className="app">
-    {/* <Header /> */}
-    <HomePage />
-  </div>
+  <Router>
+    <div className="app">
+      <Header />
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/submitclaim" component={SubmitClaimPage} />
+    </div>
+  </Router>
 );
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,21 +2,9 @@
 
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import App from './App';
-import Header from './components/Header/Header';
-import SubmitClaimPage from './containers/SubmitClaimPage/SubmitClaimPage';
 import registerServiceWorker from './registerServiceWorker';
 
-render(
-  <Router>
-    <div>
-      <Header />
-      <Route exact path="/" component={App} />
-      <Route exact path="/submitclaim" component={SubmitClaimPage} />
-    </div>
-  </Router>,
-  document.getElementById('root'),
-);
+render(<App />, document.getElementById('root'));
 registerServiceWorker();
