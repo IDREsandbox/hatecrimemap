8382c241ca2cbad09e097b2b38400a139700b712
Tanner Velten <tannervelten@gmail.com>
2018-04-19 18:34:39 -0700

    added checkboxes, removed react bootstrap

--- client/src/containers/SubmitClaimPage/SubmitClaimPage.jsx
+++ client/src/containers/SubmitClaimPage/SubmitClaimPage.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+
+import ghFilters from '../../globals/ghFilters';
 import './SubmitClaimPage.css';
 
 export default class SubmitClaimPage extends Component {
@@ -10,9 +12,18 @@ export default class SubmitClaimPage extends Component {
   }
 
   render() {
+    const checkboxes = ghFilters.map(({ name, label, key }) => (
+      <label key={key}>
+        <input type="checkbox" name={name} />
+        {label}
+      </label>
+    ));
     return (
       <div className="submitClaimPage">
         <h1>Submit a claim</h1>
+        <form className="submitClaimPage__form">
+          {checkboxes}
+        </form>
       </div>
     );
   }
