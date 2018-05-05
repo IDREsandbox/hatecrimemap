a85b440f869507d5c8bc1f0c45b787f2288230d9
Tanner Velten <tannervelten@gmail.com>
2018-05-05 12:54:51 -0700

    material ui checkboxes

--- client/src/components/GHCheckboxList/GHCheckboxList.jsx
+++ client/src/components/GHCheckboxList/GHCheckboxList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Checkbox from 'material-ui/Checkbox';
 
 import ghFilters from '../../globals/ghFilters';
 import './GHCheckboxList.css';
@@ -10,16 +11,25 @@ const GHCheckboxList = ({ onClick, showSVGs }) => {
     label,
     key,
     color,
-  }) => (
-    <label key={key}>
-      <input type="checkbox" name={name} onClick={onClick} />
-      {label}
-      {showSVGs &&
-        <svg height="12" width="12">
-          <circle cx="6" cy="6" r="6" stroke="white" opacity="1" id="meeting" fill={color} />
-        </svg>}
-    </label>
-  ));
+  }) => {
+    const labelSVG = (
+      <div>
+        {label}
+        {showSVGs &&
+          <svg height="12" width="12">
+            <circle cx="6" cy="6" r="6" stroke="white" opacity="1" id="meeting" fill={color} />
+          </svg>}
+      </div>
+    );
+    return (
+      <Checkbox
+        key={key}
+        label={labelSVG}
+        name={name}
+        onClick={onClick}
+      />
+    );
+  });
   return (
     <div className="ghCheckboxList">
       {labels}
