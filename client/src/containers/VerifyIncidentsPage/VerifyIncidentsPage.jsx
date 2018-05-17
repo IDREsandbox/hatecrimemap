a535a9d49b658ca36588247734131290d6a28501
Tanner Velten <tannervelten@gmail.com>
2018-05-17 13:20:24 -0700

    table with pagination

--- client/src/containers/VerifyIncidentsPage/VerifyIncidentsPage.jsx
+++ client/src/containers/VerifyIncidentsPage/VerifyIncidentsPage.jsx
@@ -1,5 +1,15 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {
+  Table,
+  TableBody,
+  TableFooter,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
+import Pagination from 'material-ui-pagination';
 
 import { storeMapData, getAllPoints } from '../../utils/filtering';
 
@@ -8,7 +18,9 @@ export default class App extends Component {
     super(props);
     this.state = {
       isFetching: true,
-      incidents: [],
+      incidentReports: [],
+      currentPage: 0,
+      totalPages: 0,
     };
   }
 
@@ -17,7 +29,7 @@ export default class App extends Component {
     if (allpoints.length !== 0) {
       this.setState({
         isFetching: false,
-        incidents: allpoints,
+        incidentReports: allpoints,
       });
       return;
     }
@@ -25,9 +37,10 @@ export default class App extends Component {
       .then(({ data: { mapdata } }) => {
         this.setState({
           isFetching: false,
-          incidents: storeMapData(mapdata),
+          incidentReports: storeMapData(mapdata),
+          totalPages: Math.ceil(mapdata.length / 50),
         });
-        console.log(this.state.incidents[0]);
+        console.log(this.state.incidentReports);
       })
       .catch((err) => {
         this.setState({ isFetching: false });
@@ -35,13 +48,67 @@ export default class App extends Component {
       });
   }
 
+  updatePage = (page) => {
+    this.setState({
+      currentPage: page - 1,
+    });
+  }
+
   render() {
-    const { isFetching } = this.state;
+    const { isFetching, incidentReports, currentPage, totalPages } = this.state;
+    const displayPage = currentPage + 1;
+    const lowerBound = currentPage * 50;
+    const upperBound = lowerBound + 50;
+    const displayReports = incidentReports.slice(lowerBound, upperBound);
+    let rowNum = lowerBound + 1;
+    console.log(incidentReports.length);
+    console.log(totalPages);
 
     return (
       <div className="verifyIncidentsPage">
         {!isFetching &&
-          <p>Schwoop</p>}
+          <Table
+            height="600px"
+            fixedHeader
+            selectable
+            multiSelectable
+          >
+            <TableHeader displaySelectAll={false}>
+              <TableRow>
+                <TableHeaderColumn colSpan="5" style={{ textAlign: 'center' }}>
+                  Verify Incident Reports
+                </TableHeaderColumn>
+              </TableRow>
+              <TableRow>
+                <TableHeaderColumn>Row #</TableHeaderColumn>
+                <TableHeaderColumn>ID</TableHeaderColumn>
+                <TableHeaderColumn>Harassment Location</TableHeaderColumn>
+                <TableHeaderColumn>Date of Harassment</TableHeaderColumn>
+                <TableHeaderColumn>Groups Harassed</TableHeaderColumn>
+                <TableHeaderColumn>Verification Link</TableHeaderColumn>
+              </TableRow>
+            </TableHeader>
+            <TableBody stripedRows>
+              {displayReports.map(row => (
+                <TableRow key={row.featureid}>
+                  <TableRowColumn>{rowNum++}</TableRowColumn>
+                  <TableRowColumn>{row.featureid}</TableRowColumn>
+                  <TableRowColumn>{row.locationname}</TableRowColumn>
+                  <TableRowColumn>date of harassment</TableRowColumn>
+                  <TableRowColumn>{row.groupharassedcleaned}</TableRowColumn>
+                  <TableRowColumn>{row.sourceurl}</TableRowColumn>
+                </TableRow>
+              ))}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TableRowColumn style={{ textAlign: 'center' }}>
+                  <Pagination total={totalPages} display={7} current={displayPage} onChange={page => this.updatePage(page)} />
+                </TableRowColumn>
+              </TableRow>
+            </TableFooter>
+          </Table>
+        }
       </div>
     );
   }
