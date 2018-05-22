2b831f4176744d6a8e85b8eb7ef1b9cdb29a1134
Tanner Velten <tannervelten@gmail.com>
2018-05-22 15:50:58 -0700

    switched to stateless component

--- client/src/containers/EditIncidentDialog/EditIncidentDialog.jsx
+++ client/src/containers/EditIncidentDialog/EditIncidentDialog.jsx
@@ -1,46 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 
-export default class EditIncidentDialog extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
+const EditIncidentDialog = ({ handleCloseDialog, open }) => {
+  const actions = [
+    <FlatButton
+      label="Cancel"
+      primary
+      onClick={handleCloseDialog}
+    />,
+    <FlatButton
+      label="Submit"
+      primary
+      disabled
+      onClick={handleCloseDialog}
+    />,
+  ];
 
-  render() {
-    const { handleCloseDialog, open } = this.props;
-    const actions = [
-      <FlatButton
-        label="Cancel"
-        primary
-        onClick={handleCloseDialog}
-      />,
-      <FlatButton
-        label="Submit"
-        primary
-        disabled
-        onClick={handleCloseDialog}
-      />,
-    ];
-
-    return (
-      <Dialog
-        title="Edit Entry"
-        actions={actions}
-        modal={false}
-        open={open}
-        autoScrollBodyContent
-      >
-        Edit here
-      </Dialog>
-    );
-  }
-}
+  return (
+    <Dialog
+      title="Edit Entry"
+      actions={actions}
+      modal={false}
+      open={open}
+      autoScrollBodyContent
+    >
+      Edit here
+    </Dialog>
+  );
+};
 
 EditIncidentDialog.propTypes = {
   open: PropTypes.bool.isRequired,
   handleCloseDialog: PropTypes.func.isRequired,
 };
+
+export default EditIncidentDialog;
