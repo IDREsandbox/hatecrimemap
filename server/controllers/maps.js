45a64769babd3307467242236c01dda11927442b
Tanner Velten <tannervelten@gmail.com>
2018-05-26 12:36:47 -0700

    backend for unreviewed points

--- server/controllers/maps.js
+++ server/controllers/maps.js
@@ -3,7 +3,10 @@ const express = require('express');
 const db = require('../models');
 
 const router = express.Router();
-const pointsInUSQuery = 'SELECT lon, lat, reporttype, locationname, verified, featureid, sourceurl, groupharassedcleaned, validsourceurl FROM hcmdata WHERE (lon < -66.796875 AND lon > -124.5849609375) AND (lat < 49.00905080938215 AND lat > 25.125392611512158)';
+const desiredColumns = 'lon, lat, reporttype, locationname, verified, featureid, sourceurl, groupharassedcleaned, validsourceurl';
+const pointsInUSQuery = `SELECT ${desiredColumns} FROM hcmdata WHERE (lon < -66.796875 AND lon > -124.5849609375) AND (lat < 49.00905080938215 AND lat > 25.125392611512158)`;
+const unreviewedPointsQuery = `SELECT ${desiredColumns} FROM hcmdata WHERE verified = '0'`;
+console.log(unreviewedPointsQuery);
 
 router.use((req, res, next) => {
   /* queries to /maps api go through here first */
@@ -16,6 +19,18 @@ router.get('/', (req, res) => {
 
 router.get('/usapoints', (req, res) => {
   db.any(pointsInUSQuery)
+    .then((mapdata) => {
+      res.status(200)
+        .json({
+          status: 'success',
+          mapdata,
+        });
+    })
+    .catch(err => console.log('ERROR:', err));
+});
+
+router.get('/unreviewedpoints', (req, res) => {
+  db.any(unreviewedPointsQuery)
     .then((mapdata) => {
       res.status(200)
         .json({
