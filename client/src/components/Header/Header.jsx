bc7558f3433f29b2da6adb184d180edc89ac18f3
Tanner Velten <tannervelten@gmail.com>
2018-05-28 14:51:17 -0700

    npm icons, header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,26 +1,55 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import FlatButton from 'material-ui/FlatButton';
-import IconButton from 'material-ui/IconButton';
-import ActionHome from 'material-ui/svg-icons/action/home';
-import AppBar from 'material-ui/AppBar';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import HomeIcon from '@material-ui/icons/Home';
 
-import './Header.css';
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flex: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+    color: 'white',
+  },
+  reportIncidentButton: {
+    color: 'white',
+    textDecoration: 'none',
+  },
+};
 
-const Header = () => (
-  <AppBar
-    className="headerContainer"
-    title="Mapping Harassment in the US"
-    iconElementLeft={<Link to="/"><IconButton><ActionHome /></IconButton></Link>}
-    iconElementRight={
-      <Link to="/reportincident">
-        <FlatButton
-          className="header__reportIncident"
-          label="Report Incident"
-          style={{ color: 'white' }}
-        />
-      </Link>}
-  />
+const Header = ({ classes }) => (
+  <div className={classes.root}>
+    <AppBar position="static">
+      <Toolbar>
+        <Link to="/">
+          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+            <HomeIcon />
+          </IconButton>
+        </Link>
+        <Typography variant="title" color="inherit" className={classes.flex}>
+          Mapping Harassment in the US
+        </Typography>
+        <Link to="/reportincident">
+          <Button className={classes.reportIncidentButton} color="inherit">Report Incident</Button>
+        </Link>
+      </Toolbar>
+    </AppBar>
+  </div>
+  // />
 );
 
-export default Header;
+Header.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Header);
