58d0943b6ff2cc8ba63613155b8a9a0afeb7f819
Tanner Velten <tannervelten@gmail.com>
2018-06-07 11:50:13 -0700

    removed mui provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import HomePage from './containers/HomePage/HomePage';
 import Header from './components/Header/Header';
@@ -9,16 +8,14 @@ import VerifyIncidentsPage from './containers/VerifyIncidentsPage/VerifyIncident
 import './App.css';
 
 const App = () => (
-  <MuiThemeProvider>
-    <Router>
-      <div className="app">
-        <Header />
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/reportincident" component={ReportIncidentPage} />
-        <Route exact path="/verifyincidents" component={VerifyIncidentsPage} />
-      </div>
-    </Router>
-  </MuiThemeProvider>
+  <Router>
+    <div className="app">
+      <Header />
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/reportincident" component={ReportIncidentPage} />
+      <Route exact path="/verifyincidents" component={VerifyIncidentsPage} />
+    </div>
+  </Router>
 );
 
 export default App;
