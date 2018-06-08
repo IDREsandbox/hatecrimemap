05638e7fa6f7867170ef40830746874ffc0f372b
Tanner Velten <tannervelten@gmail.com>
2018-06-07 19:56:25 -0700

    loading on home page

--- client/src/containers/HomePage/HomePage.jsx
+++ client/src/containers/HomePage/HomePage.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import MapWrapper from '../../components/MapWrapper/MapWrapper';
 import SideMenu from '../../components/SideMenu/SideMenu';
 import { updateCurrentLayers, getMapData, storeMapData, getAllPoints } from '../../utils/filtering';
 import './HomePage.css';
 
-export default class App extends Component {
+const styles = () => ({
+  progress: {
+    position: 'fixed',
+    top: '50%',
+    left: '50%',
+  },
+});
+
+class HomePage extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -55,15 +66,25 @@ export default class App extends Component {
 
   render() {
     const { isFetching, mapdata, currentLayers } = this.state;
+    const { classes } = this.props;
 
     return (
       <div className="homePage">
-        {!isFetching &&
+        {isFetching ? (
+          <CircularProgress className={classes.progress} />
+        ) : (
           <React.Fragment>
             <MapWrapper mapdata={mapdata} zoom={4} />
             <SideMenu updateMapData={this.updateMapData} resetMapData={this.resetMapData} currentLayers={currentLayers} />
-          </React.Fragment>}
+          </React.Fragment>
+        )}
       </div>
     );
   }
 }
+
+HomePage.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(HomePage);
