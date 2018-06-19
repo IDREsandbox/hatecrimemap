01012a4b0ceb08ef6952514f22daf16377a56d06
Tanner Velten <tannervelten@gmail.com>
2018-06-19 12:46:25 -0700

    unreviewed query

--- server/controllers/maps.js
+++ server/controllers/maps.js
@@ -5,12 +5,14 @@ const db = require('../models');
 const router = express.Router();
 const desiredColumns = 'lon, lat, reporttype, locationname, verified, featureid, sourceurl, groupsharassed, validsourceurl';
 const pointsInUSQuery = `SELECT ${desiredColumns} FROM hcmdata WHERE (lon < -66.796875 AND lon > -124.5849609375) AND (lat < 49.00905080938215 AND lat > 25.125392611512158)`;
-const unreviewedPointsQuery = `SELECT ${desiredColumns} FROM hcmdata WHERE verified = 0`;
-// const insertUnconfirmedPoint = `
-//   INSERT INTO hcmdata
-//   (locationname, verified)
-//   VALUES('SLO', '-1')
-// `;
+const unreviewedPointsQuery = `SELECT ${desiredColumns} FROM hcmdata WHERE verified = -1`;
+/*
+const insertUnconfirmedPoint = `
+  INSERT INTO hcmdata
+  (locationname, verified)
+  VALUES('SLO', '-1')
+`;
+*/
 
 router.use((req, res, next) => {
   /* queries to /maps api go through here first */
