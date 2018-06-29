76ed818996be65398e65a75f801bc3752ec7b664
Tanner Velten <tannervelten@gmail.com>
2018-06-29 11:34:14 -0700

    use env variables

--- server/scripts/email.js
+++ server/scripts/email.js
@@ -3,11 +3,12 @@ const nodemailer = require('nodemailer');
 const db = require('../models');
 
 const countUnreviewedQuery = 'SELECT COUNT(verified) FROM hcmdata WHERE verified = -1';
+const gmailAddress = process.env.GMAIL_ADDRESS;
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'hatemapucla@gmail.com',
+    user: gmailAddress,
     pass: process.env.GMAIL_PASSWORD,
   },
 });
@@ -16,8 +17,8 @@ function sendEmail() {
   db.one(countUnreviewedQuery)
     .then(({ count }) => {
       const mailOptions = {
-        from: 'hatemapucla@email.com',
-        to: 'albertkun@idre.ucla.edu',
+        from: gmailAddress,
+        to: gmailAddress,
         subject: `${count} unreviewed incident reports!`,
         html: 'Click <a href="http://www.hatecrimemap.com/verifyincidents" target="_blank">here</a> to review them. (but not really cause production is not up to date)',
       };
