b14f486982df0c5d3a7e70b6057d095643459f28
Tanner Velten <tannervelten@gmail.com>
2018-07-03 17:11:20 -0700

    added helmet

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 
 const routes = require('./controllers');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(helmet());
 
 // Priority serve any static files
 app.use(express.static(path.resolve(__dirname, '../client/build')));
