cb88a9696d160324c0d339623554c4f391668323
Chris Lam <christopherwlam@gmail.com>
2019-01-17 15:48:13 -0800

    Labeled get request with state name, allowed get request for state and optional category

--- server/controllers/totals.js
+++ server/controllers/totals.js
@@ -8,16 +8,34 @@ const {
 
 const router = express.Router();
 
-const columns = 'sum_harassment, jewish_harassed_total';
+const columns = 'name, sum_harassment, jewish_harassed_total';
 let category = 'jewish';
+let state = 'california';
 const updateAll = `
 update us_states set sum_harassment = (select count(*) from hcmdata where ST_Intersects(us_states.geom, ST_SetSRID(ST_MakePoint(hcmdata.lon, hcmdata.lat), 4326))),
 jewish_harassed_total = (select count(*) from hcmdata where groupsharassed ilike '%jewish%' and ST_Intersects(us_states.geom, ST_SetSRID(ST_MakePoint(hcmdata.lon, hcmdata.lat), 4326)))
+african_american_harassed_total = (select count(*) from hcmdata where groupsharassed ilike '%african american%' and ST_Intersects(us_states.geom, ST_SetSRID(ST_MakePoint(hcmdata.lon, hcmdata.lat), 4326)))
+arab_american_harassed_total = (select count(*) from hcmdata where groupsharassed ilike '%arab american%' and ST_Intersects(us_states.geom, ST_SetSRID(ST_MakePoint(hcmdata.lon, hcmdata.lat), 4326)))
+asian_american_harassed_total = (select count(*) from hcmdata where groupsharassed ilike '%jewish%' and ST_Intersects(us_states.geom, ST_SetSRID(ST_MakePoint(hcmdata.lon, hcmdata.lat), 4326)))
+disabled_harassed_total = (select count(*) from hcmdata where groupsharassed ilike '%disabled%' and ST_Intersects(us_states.geom, ST_SetSRID(ST_MakePoint(hcmdata.lon, hcmdata.lat), 4326)))
+latinx_harassed_total = (select count(*) from hcmdata where groupsharassed ilike '%latinx%' and ST_Intersects(us_states.geom, ST_SetSRID(ST_MakePoint(hcmdata.lon, hcmdata.lat), 4326)))
+lgbt_harassed_total = (select count(*) from hcmdata where groupsharassed ilike '%lgbt%' and ST_Intersects(us_states.geom, ST_SetSRID(ST_MakePoint(hcmdata.lon, hcmdata.lat), 4326)))
+muslim_harassed_total = (select count(*) from hcmdata where groupsharassed ilike '%muslim%' and ST_Intersects(us_states.geom, ST_SetSRID(ST_MakePoint(hcmdata.lon, hcmdata.lat), 4326)))
+native_american_harassed_total = (select count(*) from hcmdata where groupsharassed ilike '%native american%' and ST_Intersects(us_states.geom, ST_SetSRID(ST_MakePoint(hcmdata.lon, hcmdata.lat), 4326)))
+pacific_islander_harassed_total = (select count(*) from hcmdata where groupsharassed ilike '%pacific islander%' and ST_Intersects(us_states.geom, ST_SetSRID(ST_MakePoint(hcmdata.lon, hcmdata.lat), 4326)))
+sikh_harassed_total = (select count(*) from hcmdata where groupsharassed ilike '%sikh%' and ST_Intersects(us_states.geom, ST_SetSRID(ST_MakePoint(hcmdata.lon, hcmdata.lat), 4326)))
+women_harassed_total = (select count(*) from hcmdata where groupsharassed ilike '%women%' and ST_Intersects(us_states.geom, ST_SetSRID(ST_MakePoint(hcmdata.lon, hcmdata.lat), 4326)))
+men_harassed_total = (select count(*) from hcmdata where groupsharassed ilike '%men%' and ST_Intersects(us_states.geom, ST_SetSRID(ST_MakePoint(hcmdata.lon, hcmdata.lat), 4326)))
+girls_harassed_total = (select count(*) from hcmdata where groupsharassed ilike '%girls%' and ST_Intersects(us_states.geom, ST_SetSRID(ST_MakePoint(hcmdata.lon, hcmdata.lat), 4326)))
+boys_harassed_total = (select count(*) from hcmdata where groupsharassed ilike '%boys%' and ST_Intersects(us_states.geom, ST_SetSRID(ST_MakePoint(hcmdata.lon, hcmdata.lat), 4326)))
+white_harassed_total = (select count(*) from hcmdata where groupsharassed ilike '%white%' and ST_Intersects(us_states.geom, ST_SetSRID(ST_MakePoint(hcmdata.lon, hcmdata.lat), 4326)))
+immigrants_harassed_total = (select count(*) from hcmdata where groupsharassed ilike '%immigrants%' and ST_Intersects(us_states.geom, ST_SetSRID(ST_MakePoint(hcmdata.lon, hcmdata.lat), 4326)))
+trump_supporter_harassed_total = (select count(*) from hcmdata where groupsharassed ilike '%trump supporter%' and ST_Intersects(us_states.geom, ST_SetSRID(ST_MakePoint(hcmdata.lon, hcmdata.lat), 4326)))
 `;
 const updateCategory = `update us_states set ${category}_harassed_total = (select count(*) from hcmdata where groupsharassed ilike'%${category}%' and ST_Intersects(us_states.geom, ST_SetSRID(ST_MakePoint(hcmdata.lon, hcmdata.lat), 4326)))`
 
 const getTotals = `select ${columns} from us_states`;
-const getCategory = `select ${category}_harassed_total from us_states`;
+const getStateCategory = `select name, ${category || columns}_harassed_total from us_states where name ilike '%${state}%'`;
 
 router.use((req, res, next) => {
 	/* queries to /totals api go through here first */
@@ -25,7 +43,10 @@ router.use((req, res, next) => {
 });
 
 router.get('/', (req, res) => {
-	db.any(getTotals)
+	db.map(getTotals, [], eachState => {
+		let {[name], ...rest} = eachState;
+		return (eachState.name: rest);
+	})
 	.then((result) => {
 		res.status(200)
 		.json({
@@ -36,8 +57,13 @@ router.get('/', (req, res) => {
 	.catch(err => console.log('ERROR: ', err));
 });
 
-router.get('/:state', (req, res) => {
-	db.any(getCategory)
+router.get('/:state/:category?', (req, res) => {
+	this.category = req.params.category;
+	this.state = req.params.state;
+	db.map(getStateCategory, [], eachState => {
+		let {[name], ...rest} = eachState;
+		return (eachState.name: rest);
+	})
 	.then(result => {
 		res.status(200)
 		.json({
@@ -63,7 +89,7 @@ router.get('/update', (req, res) => {
 
 router.get('/update/:state', (req, res) => {
 	this.category = req.params.state;
-	db.one(updateAll)
+	db.one(updateCategory)
 	.then(result => {
 		res.status(200)
 		.json({
