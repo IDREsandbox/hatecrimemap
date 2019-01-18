b629446f49ae770d074b1f89b0222768ba2c2b03
Chris Lam <christopherwlam@gmail.com>
2019-01-18 10:27:00 -0800

    Updated state-totals fetching and testing updating by state (which isn't very efficient, we should have 56 state columns of totals, not ~15 totals columns of 56 states

--- server/controllers/totals.js
+++ server/controllers/totals.js
@@ -9,8 +9,6 @@ const {
 const router = express.Router();
 
 const columns = 'name, sum_harassment, jewish_harassed_total';
-var category = 'jewish';
-var state = 'california';
 const updateAll = `
 update us_states set sum_harassment = (select count(*) from hcmdata where ST_Intersects(us_states.geom, ST_SetSRID(ST_MakePoint(hcmdata.lon, hcmdata.lat), 4326))),
 jewish_harassed_total = (select count(*) from hcmdata where groupsharassed ilike '%jewish%' and ST_Intersects(us_states.geom, ST_SetSRID(ST_MakePoint(hcmdata.lon, hcmdata.lat), 4326)))
@@ -32,10 +30,6 @@ white_harassed_total = (select count(*) from hcmdata where groupsharassed ilike 
 immigrants_harassed_total = (select count(*) from hcmdata where groupsharassed ilike '%immigrants%' and ST_Intersects(us_states.geom, ST_SetSRID(ST_MakePoint(hcmdata.lon, hcmdata.lat), 4326)))
 trump_supporter_harassed_total = (select count(*) from hcmdata where groupsharassed ilike '%trump supporter%' and ST_Intersects(us_states.geom, ST_SetSRID(ST_MakePoint(hcmdata.lon, hcmdata.lat), 4326)))
 `;
-const updateCategory = `update us_states set ${category}_harassed_total = (select count(*) from hcmdata where groupsharassed ilike'%${category}%' and ST_Intersects(us_states.geom, ST_SetSRID(ST_MakePoint(hcmdata.lon, hcmdata.lat), 4326)))`
-
-const getTotals = `select ${columns} from us_states`;
-const getStateCategory = `select name, ${category || columns}_harassed_total from us_states where name ilike '%${state}%'`;
 
 router.use((req, res, next) => {
 	/* queries to /totals api go through here first */
@@ -43,7 +37,7 @@ router.use((req, res, next) => {
 });
 
 router.get('/', (req, res) => {
-	db.any(getTotals)
+	db.any(`select ${columns} from us_states`)
 	.then((result) => {
 		res.status(200)
 		.json({
@@ -55,9 +49,7 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:state/:category?', (req, res) => {
-	this.category = req.params.category;
-	this.state = req.params.state;
-	db.any(getStateCategory)
+	db.any(`select ${req.params.category ? req.params.category + '_harassed_total, name' : columns} from us_states where name ilike '%${req.params.state}%'`)
 	.then(result => {
 		res.status(200)
 		.json({
@@ -82,8 +74,7 @@ router.get('/update', (req, res) => {
 });
 
 router.get('/update/:state', (req, res) => {
-	this.category = req.params.state;
-	db.one(updateCategory)
+	db.one(updateAll + " WHERE us_states.name ilike '%" + req.params.state + "%'")
 	.then(result => {
 		res.status(200)
 		.json({
